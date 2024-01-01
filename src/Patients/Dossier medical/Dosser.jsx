9bb425a459bd36a434b727a1046edff7b3e0cdb6
Achrafgab10 <90289900+Achrafgab10@users.noreply.github.com>
2024-01-01 17:24:46 +0100

    Dossierr

--- src/Patients/Dossier medical/Dosser.jsx
+++ src/Patients/Dossier medical/Dosser.jsx
@@ -18,7 +18,7 @@ function Dosser() {
                             <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
                                 <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle" />
                                 <h2>prenom</h2>
-                                <h3>Web Designer</h3>
+                                <h3>Web Designesr</h3>
                                 <div class="social-links mt-2">
                                     <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
                                     <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
